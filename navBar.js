f1b59e74eb8ed0b25342feaaaa90a119717ade0d
matiu1998 <84108365+matiu1998@users.noreply.github.com>
2024-11-02 11:08:14 -0500

    Delete navBar.js

--- navBar.js
+++
@@ -1,7 +0,0 @@
-// navBar.js
-fetch('navBar.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('navbar-container').innerHTML = data;
-    })
-    .catch(error => console.error('Error al cargar la barra de navegación:', error));
